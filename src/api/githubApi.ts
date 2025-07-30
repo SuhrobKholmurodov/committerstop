e44410723bcc24f0bc7d66e502b46da2f3bb4d8f
suhrobKholmurodov <suhrobkholmurodov1@gmail.com>
2025-07-30 15:44:24 +0500

    feat: add GitHub token support to avoid API rate limit

--- src/api/githubApi.ts
+++ src/api/githubApi.ts
@@ -1,9 +1,19 @@
 import type { GitHubUser } from "@/types";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+const token = import.meta.env.VITE_GITHUB_TOKEN;
+
 export const githubApi = createApi({
   reducerPath: "githubApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "https://api.github.com/" }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://api.github.com/",
+    prepareHeaders: (headers) => {
+      if (token) {
+        headers.set("Authorization", `Bearer ${token}`);
+      }
+      return headers;
+    },
+  }),
   endpoints: (builder) => ({
     getGitHubUserByUsername: builder.query<GitHubUser, string>({
       query: (username) => `users/${username}`,
