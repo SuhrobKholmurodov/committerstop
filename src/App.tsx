bc29ce3541de80a2f461184ec870b12055952f1e
suhrobKholmurodov <suhrobkholmurodov1@gmail.com>
2025-10-15 11:59:27 +0500

    resolve it

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { ToastContainer } from "react-fox-toast";
 import AppRoutes from "./routes";
 import { Footer } from "./components/common";
-import Test from "./test";
 
 const App = () => {
   return (
@@ -10,7 +9,6 @@ const App = () => {
       <div className="min-h-screen bg-gradient-to-br from-gray-50 to-gray-100 dark:from-gray-900 dark:to-gray-800">
         <main className="mx-auto px-[200px] sm:px-1 sm:py-0 py-4">
           <AppRoutes />
-          <Test />
         </main>
         <Footer />
         <ToastContainer />
