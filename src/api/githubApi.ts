01fd84d5831d4a39ccc9915cf19e0030ab12f13d
suhrobKholmurodov <suhrobkholmurodov1@gmail.com>
2026-01-30 14:48:41 +0500

    refactor: remove GitHub API token handling and update base URL for local development

--- src/api/githubApi.ts
+++ src/api/githubApi.ts
@@ -1,25 +1,14 @@
 import type { GitHubUser } from "@/types";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const token = import.meta.env.GITHUB_TOKEN;
-
 export const githubApi = createApi({
   reducerPath: "githubApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://api.github.com/",
-    prepareHeaders: (headers) => {
-      if (token) {
-        headers.set("Authorization", `Bearer ${token}`);
-      }
-      headers.set("Accept", "application/vnd.github.v3+json");
-      return headers;
-    },
+    baseUrl: "http://localhost:3000/",
   }),
-  keepUnusedDataFor: 86400,
   endpoints: (builder) => ({
     getGitHubUserByUsername: builder.query<GitHubUser, string>({
-      query: (username) => `users/${username}`,
-      keepUnusedDataFor: 86400,
+      query: (username) => `github-user/${username}`,
     }),
   }),
 });
